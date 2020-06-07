6fa1e01dc6b720b27adbd907dfc40af3d6124305
Akshay Nair <phenax5@gmail.com>
2020-06-07 20:27:38 +0530

    removes padding

--- blocks.h
+++ blocks.h
@@ -1,9 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/ /*Command*/  /*Update Interval*/ /*Update Signal*/
-  //{"", "cat ~/.pacupdate | sed /📦0/d",     0,  9},
-  //{"🌡", "sensors | awk '/^temp1:/{print $2}'",    5,  0},
-
   {"", "~/scripts/menuitems.sh volume",             0,   10},
   {"", "~/scripts/menuitems.sh music",              0,   11},
   {"", "~/scripts/menuitems.sh brightness",         0,   12},
