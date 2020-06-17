b9ea729598f6161f4bfa3b7ecda29ca2f2eaec02
Akshay Nair <phenax5@gmail.com>
2020-06-17 19:39:22 +0530

    Adds Key mode block

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/ /*Command*/  /*Update Interval*/ /*Update Signal*/
+  {"", "~/scripts/menuitems.sh keymode",            0,   13},
   {"", "~/scripts/menuitems.sh volume",             0,   10},
   {"", "~/scripts/menuitems.sh music",              0,   11},
   {"", "~/scripts/menuitems.sh brightness",         0,   12},
