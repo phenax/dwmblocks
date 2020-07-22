1b7209ba919e0213521c88a8a85a2284ae5dac90
Akshay Nair <phenax5@gmail.com>
2020-07-22 23:40:35 +0530

    Fixes music block

--- blocks.h
+++ blocks.h
@@ -1,14 +1,14 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+#define MENUITEM(x) ("~/scripts/menuitems.sh " #x)
+
 static const Block blocks[] = {
-  /*Icon*/ /*Command*/  /*Update Interval*/ /*Update Signal*/
-  {"", "~/scripts/menuitems.sh keymode",            0,   13},
-  {"", "~/scripts/menuitems.sh volume",             0,   10},
-  {"", "~/scripts/menuitems.sh music",              0,   11},
-  {"", "~/scripts/menuitems.sh brightness",         0,   12},
-  {"", "~/scripts/menuitems.sh date",               5,   0},
-  {"", "~/scripts/menuitems.sh battery",            10,  0},
+  /*Icon*/ /*Command*/       /*Interval*/ /*Signal*/
+  {"", MENUITEM(keymode),            0,      13},
+  {"", MENUITEM(volume),             0,      10},
+  {"", MENUITEM(music),              10,     11},
+  {"", MENUITEM(brightness),         0,      12},
+  {"", MENUITEM(date),               5,      0},
+  {"", MENUITEM(battery),            10,     0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '|';
 
