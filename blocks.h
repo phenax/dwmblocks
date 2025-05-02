50bde2847bc07e317fe808423db9ceddbd07aa3a
Akshay Nair <phenax5@gmail.com>
2025-05-02 16:27:15 +0530

    Add music block

--- blocks.h
+++ blocks.h
@@ -3,11 +3,11 @@
 static const Block blocks[] = {
   /*Icon*/ /*Command*/       /*Interval*/ /*Signal*/
   {"", MENUITEM(volume),             10,      10},
+  {"", MENUITEM(music),              10,      11},
   {"", MENUITEM(network),            10,      14},
   {"", MENUITEM(date),               5,       0},
   {"", MENUITEM(battery),            10,      0},
   // {"", MENUITEM(keymode),            0,      13},
-  // {"", MENUITEM(music),              10,     11},
   // {"", MENUITEM(brightness),         0,      12},
 };
 
